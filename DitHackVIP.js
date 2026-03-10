00913a2d331dfd29db1a87f0ef00bd3a78bfeca1
xDitt4GT++ <212197434+AdithyaWijaya@users.noreply.github.com>
2026-03-10 20:35:53 +0800

    Update DitHackVIP.js

--- DitHackVIP.js
+++ DitHackVIP.js
@@ -11,7 +11,7 @@
 
     // License configuration
     const LICENSE_CONFIG = {
-        licenseFile: 'https://adithyawijaya.github.io/DitHack-Script/xditt4gt.json'
+        licenseFile: 'https://dithackvip.vercel.app/xditt4gt.json'
     };
 
     // Validate license against JSON file
@@ -1061,4 +1061,5 @@
 
     console.log('✅ DitHack loaded!');
 })();
+
 
